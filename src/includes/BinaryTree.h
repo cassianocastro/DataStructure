49f318cb7675b1e7145d6ad2cf75ba1097c6afd5
Cassiano Castro <development.castro@gmail.com>
2024-01-29 17:39:55 -0300

    Inclusion of std headers and type definitions

--- src/includes/BinaryTree.h
+++ src/includes/BinaryTree.h
@@ -2,6 +2,21 @@
 
 #define BINARY_TREE_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <iso646.h>
+#include <stdbool.h>
 
+/**
+ *
+ */
+typedef struct nodo
+{
+    char info;
+    struct nodo* esquerda;
+    struct nodo* direita;
+}
+nodo;
 
 #endif
