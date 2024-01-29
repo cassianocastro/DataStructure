50766fbed9a96270d8751c4c4203cedbe75f7eb8
Cassiano Castro <development.castro@gmail.com>
2024-01-29 17:19:54 -0300

    Style: format adjust

--- src/ArvoreBinaria.c
+++ src/ArvoreBinaria.c
@@ -9,29 +9,38 @@ typedef struct nodo
     char info;
     struct nodo* esquerda;
     struct nodo* direita;
-} nodo;
+}
+nodo;
 
 nodo* createNodo(char info)
 {
     nodo* novo     = malloc( sizeof(nodo) );
+
     novo->info     = info;
     novo->esquerda = NULL;
     novo->direita  = NULL;
+
     return novo;
 }
 
 //Inclusao de um nodo na Arvore
 void insere_folha(nodo** arv, char info)
 {
-    if ( *arv == NULL ) {
+    if ( *arv == NULL )
+    {
         *arv = createNodo(info);
-    } else {
+    }
+    else
+    {
         //se a informacao e menor que a informacao da arvore, insere a esquerda
-        if ( info < (*arv)->info ) {
+        if ( info < (*arv)->info )
+        {
             puts("Inserindo à Esquerda...");
             insere_folha(&(*arv)->esquerda, info);
             //se a informacao e maior que a informacao da arvore, insere a direita
-        } else if ( info > (*arv)->info ) {
+        }
+        else if ( info > (*arv)->info )
+        {
             puts("Inserindo à Direita...");
             insere_folha(&(*arv)->direita, info);
         }
@@ -41,13 +50,18 @@ void insere_folha(nodo** arv, char info)
 //Caminhamento pre-ordem
 void pre_ordem(nodo* arv)
 {
-    if ( arv != NULL ) {
+    if ( arv != NULL )
+    {
         printf("%c \n", arv->info);
-        if ( arv->esquerda != NULL ) {
+
+        if ( arv->esquerda != NULL )
+        {
             puts("Nodo Esquerdo.");
             pre_ordem(arv->esquerda);
         }
-        if ( arv->direita != NULL ) {
+
+        if ( arv->direita != NULL )
+        {
             puts("Nodo Direito.");
             pre_ordem(arv->direita);
         }
@@ -57,13 +71,18 @@ void pre_ordem(nodo* arv)
 //Caminhamento em-ordem
 void em_ordem(nodo* arv)
 {
-    if ( arv != NULL ) {
-        if ( arv->esquerda != NULL ) {
+    if ( arv != NULL )
+    {
+        if ( arv->esquerda != NULL )
+        {
             puts("Nodo Esquerdo.");
             em_ordem(arv->esquerda);
         }
+
         printf("%c \n", arv->info);
-        if ( arv->direita != NULL ) {
+
+        if ( arv->direita != NULL )
+        {
             puts("Nodo Direito.");
             em_ordem(arv->direita);
         }
@@ -73,34 +92,47 @@ void em_ordem(nodo* arv)
 //Caminhamento pos-ordem
 void pos_ordem(nodo * arv)
 {
-    if ( arv != NULL ) {
-        if ( arv->esquerda != NULL ) {
+    if ( arv != NULL )
+    {
+        if ( arv->esquerda != NULL )
+        {
             puts("Nodo Esquerdo.");
             pos_ordem(arv->esquerda);
         }
-        if ( arv->direita != NULL ) {
+
+        if ( arv->direita != NULL )
+        {
             puts("Nodo direito.");
             pos_ordem(arv->direita);
         }
+
         printf("%c\n", arv->info);
     }
 }
 
 void pesquisar_nodo(nodo* arv, char pesq)
 {
-    if ( arv != NULL ) {
-        if ( arv->info == pesq ) {
+    if ( arv != NULL )
+    {
+        if ( arv->info == pesq )
+        {
             printf("Nodo encontrado: %c\n", arv->info);
-        } else {
-            if ( pesq > arv->info ) {
+        }
+        else
+        {
+            if ( pesq > arv->info )
+            {
                 puts("Pesquisando a direita...");
                 pesquisar_nodo(arv->direita, pesq);
-            } else {
+            }
+            else
+            {
                 puts("Pesquisando a esquerda...");
                 pesquisar_nodo(arv->esquerda, pesq);
             }
         }
-    } else
+    }
+    else
         puts("Nodo não encontrado.");
 }
 
@@ -131,7 +163,9 @@ int main(int argc, char const *argv[])
 	{
         createMenu();
         scanf( "%i", &option );
+
         system( "clear" );
+
         switch ( option )
 		{
             case 1:
@@ -140,7 +174,11 @@ int main(int argc, char const *argv[])
 				{
                     printf("Informe a letra a inserir na Árvore (ou @ para sair): ");
                     scanf( " %c", &info );
-                    if ( info != '@' ) insere_folha(&arv, info);
+
+                    if ( info != '@' )
+                    {
+                        insere_folha(&arv, info);
+                    }
                 }
                 break;
             case 2:
@@ -155,6 +193,7 @@ int main(int argc, char const *argv[])
             case 5:
                 printf("Informe a letra a ser pesquisada na Árvore: ");
                 scanf(" %c", &info);
+
                 pesquisar_nodo(arv, info);
                 break;
             case 0:
@@ -164,5 +203,6 @@ int main(int argc, char const *argv[])
                 puts("Opção inválida!");
         }
     }
+
     return ( EXIT_SUCCESS );
 }
