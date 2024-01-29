0871cb0e27d5fd0c75820272724d2d89acd90dc1
Cassiano Castro <development.castro@gmail.com>
2024-01-29 18:09:35 -0300

    Added typedef statements and function prototypes

--- src/includes/DequeExample.h
+++ src/includes/DequeExample.h
@@ -2,6 +2,62 @@
 
 #define DEQUE_EXAMPLE_H
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <iso646.h>
+#include <locale.h>
 
+/**
+ *
+ */
+typedef struct No
+{
+    int idade;
+
+    struct No* proximo;
+    struct No* anterior;
+}
+No;
+
+/**
+ *
+ */
+No* createElement(int);
+
+/**
+ *
+ */
+No* setFirst(No*, No*);
+
+/**
+ *
+ */
+No* getLastElement(No*);
+
+/**
+ *
+ */
+No* setLast(No*, No*);
+
+/**
+ *
+ */
+void showList(No*);
+
+/**
+ *
+ */
+void showReversed(No*);
+
+/**
+ *
+ */
+No* removeElement(No*, int);
+
+/**
+ *
+ */
+No* add(No*, No*, int);
 
 #endif
