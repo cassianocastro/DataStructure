52b2cc051193290343953d2495798560ea465438
Cassiano Castro <development.castro@gmail.com>
2024-01-30 15:47:35 -0300

    Style: format adjust

--- src/app/OperacoesDeFila.c
+++ src/app/OperacoesDeFila.c
@@ -4,23 +4,35 @@
 #include <iso646.h>
 #include <locale.h>
 //OK
-typedef enum {
-    EXIT, CREATE, DELETE, SHOW
-} Options;
+typedef enum
+{
+    EXIT,
+    CREATE,
+    DELETE,
+    SHOW
+}
+Options;
 
-typedef struct {
+typedef struct
+{
     int value;
+
     struct element* pointerToNext;
-} element;
+}
+element;
 
 //Definicao do descritor. Ponteiros que armazezam o inicio e o final da fila
-typedef struct {
+typedef struct
+{
     struct element* pointerToBeginQueue;
     struct element* pointerToEndQueue;
-} descritor;
+}
+descritor;
 
-element* createElement( void ){
+element* createElement( void )
+{
     int value = 0;
+
     printf ("Informe um valor para o novo elemento da fila: ");
     scanf  ("%d", &value);
 
@@ -31,55 +43,72 @@ element* createElement( void ){
     return newElement;
 }
 
-bool isEmpty(descritor* desc){
+bool isEmpty(descritor* desc)
+{
     if ( desc->pointerToBeginQueue == NULL )
+    {
         return true;
+    }
 
     return false;
 }
 
-void enQueue(descritor* desc, element* newElement){
-    if ( isEmpty(desc) ) {
+void enQueue(descritor* desc, element* newElement)
+{
+    if ( isEmpty(desc) )
+    {
         desc->pointerToBeginQueue = newElement;
         desc->pointerToEndQueue   = newElement;
     }
-    else {
+    else
+    {
         element* lastElement       = desc->pointerToEndQueue;
         lastElement->pointerToNext = newElement;
         desc->pointerToEndQueue    = newElement;
     }
 }
 
-void deQueue(descritor* desc){
-    if ( not isEmpty(desc) ) {
+void deQueue(descritor* desc)
+{
+    if ( not isEmpty(desc) )
+    {
         element* helper           = desc->pointerToBeginQueue;
         desc->pointerToBeginQueue = helper->pointerToNext;
 
-     //Se o elemento era unico na fila, atualiza o final da fila tamb�m para NULL:
+        // Se o elemento era unico na fila, atualiza o final da fila tamb�m para NULL:
         if ( desc->pointerToBeginQueue == NULL )
+        {
             desc->pointerToEndQueue = NULL;
+        }
 
         helper->pointerToNext = NULL;
+
         printf("Valor do elemento excluído: %d \n", helper->value);
         free(helper);
-    } else
+    }
+    else
         puts("A fila está vazia!");
 }
 
-void showQueue(descritor* desc){
+void showQueue(descritor* desc)
+{
     element* firstElement = desc->pointerToBeginQueue;
 
-    if ( firstElement != NULL ){
+    if ( firstElement != NULL )
+    {
         element* element = firstElement;
+
         do {
             printf(" %d ", element->value);
             element = element->pointerToNext;
         } while ( element != NULL );
-    }else
+    }
+    else
         puts("Fila Vazia.");
 }
 
-void createMenu( void ){
+void createMenu( void )
+{
     printf (
         "\n Operações sobre Filas \n"
         "Selecione a opção desejada: \n"
@@ -88,8 +117,10 @@ void createMenu( void ){
     );
 }
 
-int main( void ){
+int main( void )
+{
     setlocale(LC_ALL, "portuguese-brazilian");
+
     Options option;
 
     element* newElement       = NULL;
@@ -97,11 +128,15 @@ int main( void ){
     desc->pointerToBeginQueue = NULL;
     desc->pointerToEndQueue   = NULL;
 
-    while ( true ) {
+    while ( true )
+    {
         createMenu();
         scanf ( "%d", &option );
+
         system("clear");
-        switch( option ) {
+
+        switch ( option )
+        {
             case EXIT:
                 exit( EXIT_SUCCESS );
                 break;
