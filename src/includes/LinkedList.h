f6aad2015161491a82ac7f86c930a63fcfc8353b
Cassiano Castro <development.castro@gmail.com>
2024-01-30 15:06:54 -0300

    Added type definitions and functions prototypes

--- src/includes/LinkedList.h
+++ src/includes/LinkedList.h
@@ -2,6 +2,50 @@
 
 #define LINKED_LIST_H
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <locale.h>
+#include <iso646.h>
 
+/**
+ *
+ */
+typedef struct No
+{
+    int idade;
+
+    struct No* prox;
+}
+No;
+
+/**
+ *
+ */
+No* createElement(int);
+
+/**
+ *
+ */
+No* setFirst(No*, No*);
+
+/**
+ *
+ */
+No* setLast(No*, No*);
+
+/**
+ *
+ */
+No* delElement(No*, int);
+
+/**
+ *
+ */
+void showList(No*);
+
+/**
+ *
+ */
+No* setIndex(No*, No*, int);
 
 #endif
