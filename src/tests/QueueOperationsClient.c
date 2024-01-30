333cd10e4969aad5263a4140c0635fa6e87c2a42
Cassiano Castro <development.castro@gmail.com>
2024-01-30 16:00:29 -0300

    Defined main() function

---
+++ src/tests/QueueOperationsClient.c
@@ -0,0 +1,41 @@
+#include "../includes/QueueOperations.h"
+
+int main(void)
+{
+    setlocale(LC_ALL, "portuguese-brazilian");
+
+    Options option;
+
+    element* newElement = NULL;
+    descritor* desc = (descritor*) malloc(sizeof(descritor));
+
+    desc->pointerToBeginQueue = NULL;
+    desc->pointerToEndQueue   = NULL;
+
+    while ( true )
+    {
+        createMenu();
+        scanf("%d", &option);
+
+        system("clear");
+
+        switch ( option )
+        {
+            case EXIT:
+                exit(EXIT_SUCCESS);
+                break;
+            case CREATE:
+                newElement = createElement();
+                enQueue(desc, newElement);
+                break;
+            case DELETE:
+                deQueue(desc);
+                break;
+            case SHOW:
+                showQueue(desc);
+                break;
+            default:
+                puts("Opção inválida!");
+        }
+    }
+}
