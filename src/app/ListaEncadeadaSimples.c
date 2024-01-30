dc8cadbea846ccfa4fad2a770243e0c645bd314b
Cassiano Castro <development.castro@gmail.com>
2024-01-30 15:02:52 -0300

    Style: removed unnecessary whitespaces

--- src/app/ListaEncadeadaSimples.c
+++ src/app/ListaEncadeadaSimples.c
@@ -13,7 +13,7 @@ No;
 
 No* createElement(int idade)
 {
-    No* novo = (No*) malloc( sizeof(No) );
+    No* novo = (No*) malloc(sizeof(No));
 
     novo->idade = idade;
     novo->prox  = NULL;
@@ -135,6 +135,7 @@ int main(int argc, char const *argv[])
     setlocale(LC_ALL, "");
 
 	No* lista = NULL;
+
     No* n     = createElement(35);
     No* n2    = createElement(50);
     No* n3    = createElement(30);
@@ -142,33 +143,40 @@ int main(int argc, char const *argv[])
     No* n5    = createElement(3);
 
     puts("\nInserindo na última posição da lista os elementos 35 e 50:");
+
     lista = setLast(lista, n);
     lista = setLast(lista, n2);
     showList(lista);
 
     puts("\nInserindo 30 como primeiro elemento da lista:");
+
     lista = setFirst(lista, n3);
     showList(lista);
 
     puts("\nInserindo 55 como ultimo elemento da lista:");
+
     lista = setLast(lista, n4);
     showList(lista);
 
     puts("\nInserindo elemento 3 na posicao 3:");
+
     lista = setIndex(lista, n5, 3);
     showList(lista);
 
     puts("\nRemovendo o elemento da posição 1 da lista:");
+
     lista = delElement(lista, 1);
     showList(lista);
 
     puts("\nRemovendo o elemento da posição 2 da lista:");
+
     lista = delElement(lista, 2);
     showList(lista);
 
     puts("\nRemovendo o elemento da posição 3 da lista:");
+
     lista = delElement(lista, 3);
     showList(lista);
 
-    return ( EXIT_SUCCESS );
+    return EXIT_SUCCESS;
 }
