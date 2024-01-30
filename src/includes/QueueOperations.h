9ad4b8a565a16babde8a83f793a72ea7eb37a57e
Cassiano Castro <development.castro@gmail.com>
2024-01-30 15:57:46 -0300

    Function declarations and type definitions

--- src/includes/QueueOperations.h
+++ src/includes/QueueOperations.h
@@ -2,6 +2,74 @@
 
 #define QUEUE_OPEARTIONS_H
 
+#include <iso646.h>
+#include <locale.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+/**
+ *
+ */
+typedef enum
+{
+    EXIT,
+    CREATE,
+    DELETE,
+    SHOW
+}
+Options;
+
+/**
+ *
+ */
+typedef struct
+{
+    int value;
+
+    struct element* pointerToNext;
+}
+element;
+
+/**
+ * Definicao do descritor.
+ * Ponteiros que armazezam o inicio e o final da fila
+ */
+typedef struct
+{
+    struct element* pointerToBeginQueue;
+    struct element* pointerToEndQueue;
+}
+descritor;
+
+/**
+ *
+ */
+element* createElement(void);
+
+/**
+ *
+ */
+bool isEmpty(descritor*);
+
+/**
+ *
+ */
+void enQueue(descritor*, element*);
+
+/**
+ *
+ */
+void deQueue(descritor*);
+
+/**
+ *
+ */
+void showQueue(descritor*);
+
+/**
+ *
+ */
+void createMenu(void);
 
 #endif
