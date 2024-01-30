6dedb1bdd6c2d5fe6300001f3f0dde8a357defd5
Cassiano Castro <development.castro@gmail.com>
2024-01-30 14:50:49 -0300

    Added content

--- src/includes/Deque.h
+++ src/includes/Deque.h
@@ -2,6 +2,62 @@
 
 #define DEQUE_H
 
+#include <iso646.h>
+#include <locale.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+/**
+ *
+ */
+typedef struct No
+{
+    int idade;
+
+    struct No* proximo;
+    struct No* anterior;
+}
+No;
+
+/**
+ *
+ */
+No* createElement(int);
+
+/**
+ * Insert in the beginning of the list
+ */
+No* setFirst(No*, No*);
+
+/**
+ *
+ */
+No* getLastElement(No*);
+
+/**
+ *
+ */
+No* setLast(No*, No*);
+
+/**
+ *
+ */
+void showList(No*);
+
+/**
+ *
+ */
+void showReversed(No*);
+
+/**
+ *
+ */
+No* removeElement(No*, int);
+
+/**
+ *
+ */
+No* add(No*, No*, int);
 
 #endif
