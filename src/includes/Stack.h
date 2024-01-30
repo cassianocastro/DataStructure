85c39264b284fa153b0a95e7c3499295fdaf63e2
Cassiano Castro <development.castro@gmail.com>
2024-01-29 23:42:46 -0300

    Defined structs and function prototypes

--- src/includes/Stack.h
+++ src/includes/Stack.h
@@ -2,6 +2,63 @@
 
 #define STACK_H
 
+#include <iso646.h>
+#include <locale.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+/**
+ *
+ */
+typedef enum
+{
+    EXIT,
+    PUSH,
+    POP,
+    SHOWSTACK
+}
+Options;
+
+/**
+ *
+ */
+typedef struct
+{
+    int valor;
+
+    struct element* anterior;
+}
+element;
+
+/**
+ *
+ */
+element* createElement(element*, int);
+
+/**
+ * Add elements in the stack
+ */
+element* push(element*, int);
+
+/**
+ *
+ */
+bool isEmpty(element*);
+
+/**
+ *
+ */
+element* pop(element*);
+
+/**
+ *
+ */
+void showStack(element*);
+
+/**
+ *
+ */
+void createMenu(void);
 
 #endif
